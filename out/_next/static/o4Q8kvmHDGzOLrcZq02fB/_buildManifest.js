4bfb1305687c4e83082028373c422edb6ba3ba1e
Griffin <griffincriste@gmail.com>
2023-09-08 13:33:49 -0400

    fixed build

---
+++ out/_next/static/o4Q8kvmHDGzOLrcZq02fB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/849-d41a42d69779bee4.js","static/chunks/pages/index-889713a8fb47bde3.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
